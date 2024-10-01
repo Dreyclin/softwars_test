f3841e6aa536aece2989ec9ab6b0150a59b7b31a
Maxim Sur <makarka16@gmail.com>
2024-10-02 00:28:34 +0300

    Styled: main section

---
+++ src/components/Aside/Item.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const Item: React.FC = () => {
+    return (
+        <div className="todo-item">
+            <h5 className="todo-item-title main-text">Нова нотатка</h5>
+            <p className="todo-item-description secondary-text">
+                <span className="todo-item-date">12:47</span>
+                <span className="todo-item-text">Ще немає тексту</span>
+            </p>
+        </div>
+    )
+}
+
+export default Item
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,12 +1,13 @@
 import React from "react";
+import "./Main.css"
 
 const Main: React.FC = () => {
     return (
         <main>
-            <p className="date"></p>
+            <p className="date">18 травня 2024 12:34</p>
             <div className="add-todo-container">
-                <input type="text" placeholder="Заголовок" />
-                <textarea name="" id="" placeholder='Текст'></textarea>
+                <input type="text" placeholder="Заголовок" className="title-field field"/>
+                <textarea name="" id="" placeholder='Текст' className="description-field field"></textarea>
             </div>
         </main>
     )
