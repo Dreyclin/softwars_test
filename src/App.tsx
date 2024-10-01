370be34acbf514cdf1ea0583187371670ac305c5
Maxim Sur <makarka16@gmail.com>
2024-10-01 16:27:52 +0300

    Add: main section layout

--- src/App.tsx
+++ src/App.tsx
@@ -19,13 +19,21 @@ function App() {
 
       <aside>
         <div className="todo-item">
-          <h5 className="todo-item-title"></h5>
+          <h5 className="todo-item-title">Нова нотатка</h5>
           <p className="todo-item-description">
             <span className="todo-item-date"></span>
             <span className="todo-item-text"></span>
           </p>
         </div>
       </aside>
+
+      <main>
+        <p className="date"></p>
+        <div className="add-todo-container">
+          <input type="text" placeholder="Заголовок"/>
+          <textarea name="" id="" placeholder='Текст'></textarea>
+        </div>
+      </main>
     </div>
   );
 }
