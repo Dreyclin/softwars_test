dcbc455ec772b0944bc1e68e991683fa57283363
Maxim Sur <makarka16@gmail.com>
2024-10-01 16:12:58 +0300

    Add: header layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,24 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div className="container">
+      <header>
+        <div className="logo">
+          <div className="logo-img"></div>
+          <div className="logo-text"></div>
+        </div>
+        <div className="control-btns">
+          <button className="btn create-new-btn"></button>
+          <button className="btn delete-btn"></button>
+        </div>
+        <div className="filter-input">
+          <input type="text" placeholder='Пошук' />
+        </div>
+      </header>
+      
+      <aside>
+
+      </aside>
     </div>
   );
 }
